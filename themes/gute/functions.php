5de326407ff2703adf9d7c5ff5130cb9db6d6378
Tomas Mulder <tcmulder@gmail.com>
2026-03-09 07:46:56 -0600

    Update lockdown

--- themes/gute/functions.php
+++ themes/gute/functions.php
@@ -1,7 +1,5 @@
 <?php
-/**
- * Automatically populate the content of the gutenberg test page.
- */
+// Automatically populate the content of the gutenberg test page.
 add_action( 'after_switch_theme', function() {
     $content = '<!-- wp:heading {"level":1} -->
 <h1 class="wp-block-heading">Your Heading Here</h1>
@@ -17,4 +15,29 @@ add_action( 'after_switch_theme', function() {
         'post_content' => $content,
         'post_status'  => 'draft',
     ] );
+} );
+
+
+// Remove all top-level admin menu items except Pages
+add_action( 'admin_menu', function() {
+    global $menu, $submenu;
+    $keep = [ 'edit.php?post_type=page' ];
+    foreach ( $menu as $key => $item ) {
+        if ( ! empty( $item[2] ) && ! in_array( $item[2], $keep, true ) ) {
+            remove_menu_page( $item[2] );
+        }
+    }
+    $submenu = [];
+}, 999 );
+
+// Hide the admin toolbar on the front-end
+add_filter( 'show_admin_bar', '__return_false' );
+
+// Redirect any wp-admin page that isn't the block editor or Pages list
+add_action( 'current_screen', function( $screen ) {
+    $allowed_bases = [ 'edit', 'post' ];
+    if ( ! in_array( $screen->base, $allowed_bases, true ) ) {
+        wp_safe_redirect( admin_url( 'edit.php?post_type=page' ) );
+        exit;
+    }
 } );
