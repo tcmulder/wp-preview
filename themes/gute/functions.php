9ae5e8872b060aff6f417b1dae70810269a78d36
Tomas Mulder <tcmulder@gmail.com>
2026-04-09 07:37:45 -0600

    Update content

--- themes/gute/functions.php
+++ themes/gute/functions.php
@@ -2,11 +2,11 @@
 // Automatically populate the content of the gutenberg test page.
 add_action( 'after_switch_theme', function() {
     $content = '<!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading">Your Heading Here</h1>
+<h1 class="wp-block-heading">Welcome to Gutenberg!</h1>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p>Your paragraph text goes here. Click any block to start editing!</p>
+<p>You can add and edit blocks here. Click any block to start editing!</p>
 <!-- /wp:paragraph -->';
 
     wp_update_post( [
